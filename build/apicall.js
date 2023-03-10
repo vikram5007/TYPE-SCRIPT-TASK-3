3a6977a91773ad1d8c66c168f1c177f2c0f13e44
VIKRAM5007 <raghulkalai92@gmail.com>
2023-03-10 09:53:01 +0530

    final working

--- build/apicall.js
+++ build/apicall.js
@@ -2,12 +2,10 @@
 function getProduct_info() {
     return fetch('/product.json')
         .then(res => res.json())
-        .then(res => {
-        return res;
-    });
+        .then(res => { return res; });
 }
 getProduct_info()
     .then(product => {
     //map- to loop through all the elements from the json
-    console.log(product.map(p => p.product_id + ' ' + p.product_name + ' ' + p.product_price + ' ' + p.product_quantity).toString());
+    console.log(product.map(p => p.productid + ' ' + p.product_name + ' ' + p.price + ' ' +p.quantity).toString());
 });
